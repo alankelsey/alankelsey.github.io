0493499d3487af3c31565a898d3802aaff3a4f8a
Alan Kelsey <akelsey@alans-mbp.mynetworksettings.com>
2024-09-22 22:04:49 -0600

    added ls command

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
     baseUrl: 'https://alankelsey.github.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
