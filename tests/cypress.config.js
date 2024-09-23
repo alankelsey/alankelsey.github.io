78c659499051a1941c7b8152f8a22599be52e335
Alan Kelsey <akelsey@alans-mbp.mynetworksettings.com>
2024-09-23 15:23:00 -0600

    enabled support file

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: false,
+    // supportFile: false,
     specPattern: '**/*.js',
     baseUrl: 'https://alankelsey.github.io',
     setupNodeEvents(on, config) {
