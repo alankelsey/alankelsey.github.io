e454d41f487a6c9fa94233e95874fdc2fb7c6442
Alan Kelsey <akelsey@alans-mbp.mynetworksettings.com>
2024-09-23 18:08:20 -0600

    added to readme and cleaned up config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # interviewPrep
+
+[![example-basic](https://github.com/alankelsey/alankelsey.github.io/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/alankelsey/alankelsey.github.io/actions/workflows/main.yml)
--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // github action path
+    // github action path : remove for local cypress runs
     supportFile: 'tests/cypress/support/e2e.{js,jsx,ts,tsx}',
-    //local work
-    // supportFile: '/cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: '**/*.js',
     baseUrl: 'https://alankelsey.github.io',
     setupNodeEvents(on, config) {
